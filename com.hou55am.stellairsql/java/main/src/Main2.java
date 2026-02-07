de1800bb8edf6360f16926799086d0e4fe8dbbec
SuperUseRYOS <42545976+dev-houssam@users.noreply.github.com>
2026-02-07 23:47:23 +0100

    Create Main2.java

---
+++ com.hou55am.stellairsql/java/main/src/Main2.java
@@ -0,0 +1,12 @@
+package com.hou55am.stellairsql;
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+ 
+public class Main2 {
+    public static void main(String[] args) {
+      
+    }
+}
